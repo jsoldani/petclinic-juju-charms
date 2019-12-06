dbf8c68469379d8cf7866366a9c2b167572eab00
jsoldani <jsoldani@gmail.com>
2019-12-06 13:22:14 +0100

    first version

--- layer-petclinic-single/reactive/layer-petclinic.py
+++
@@ -1,7 +0,0 @@
-from charmhelpers.core.hookenv import status_set
-from charms.reactive import when, when_not, set_state
-
-@when_not('petclinic.installed')
-def install_petclinic():
-	set_flag('petclinic.installed')
-	status_set('stocazzo')
